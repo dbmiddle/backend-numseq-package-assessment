5052af500fa30721f9e95a85035bd94d83f1d9c0
dbmiddle <dbmiddle39@gmail.com>
2020-03-19 16:04:45 -0400

    completed number sequence code to get tests to pass

---
+++ numseq/geo.py
@@ -0,0 +1,13 @@
+def square(n):
+    '''Return n squared'''
+    return n*n
+
+
+def triangle(n):
+    '''Returns the nth term of the numbers that can be arranged in triangular geometric shapes'''
+    return (n*(n+1))/2
+
+
+def cube(n):
+    '''Return n cubed'''
+    return n**3
